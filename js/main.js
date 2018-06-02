09206f8a1a84ff055502d995fb4a194113e142a1
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-06-02 21:08:23 +0300

    Add next, back buttons and handle buttons click

--- js/main.js
+++ js/main.js
@@ -4,10 +4,31 @@ const mainElement = document.querySelector(`.central`);
 
 const selectTemplate = (template) => {
   mainElement.innerHTML = ``;
-  mainElement.appendChild(template);
+  mainElement.appendChild(template.cloneNode(true));
 };
 
 const templates = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
 
 selectTemplate(templates[0]);
 
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? templates.length - 1 : index;
+  index = index >= templates.length ? 0 : index;
+  current = index;
+  selectTemplate(templates[current]);
+};
+
+const backBtn = document.querySelector(`.arrows__wrap button`);
+
+backBtn.addEventListener(`click`, () => {
+  current = current - 1;
+  select(current);
+});
+
+const nextBtn = document.querySelector(`.arrows__wrap button:nth-child(3)`);
+
+nextBtn.addEventListener(`click`, () => {
+  current = current + 1;
+  select(current);
+});
