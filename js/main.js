c99ce470d7891eddb258042d7525a9028a787eba
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-06-04 22:21:20 +0300

    Create back and next buttons with js

--- js/main.js
+++ js/main.js
@@ -33,6 +33,44 @@ const countKeyboardClicks = () => {
 
 countKeyboardClicks();
 
+const createNextBtn = () => {
+  const nextBtn = document.createElement(`button`);
+  nextBtn.className = `arrows__btn`;
+  nextBtn.innerHTML = `->`;
+  return nextBtn;
+};
+
+const createBackBtn = () => {
+  const backBtn = document.createElement(`button`);
+  backBtn.className = `arrows__btn`;
+  backBtn.innerHTML = `<-`;
+  return backBtn;
+};
+
+const addButtonsWithStyles = () => {
+  const divEl = document.createElement(`div`);
+  divEl.className = `arrows__wrap`;
+
+  const style = document.createElement(`style`);
+  style.type = `text/css`;
+
+  let css = `.arrows__wrap { position: absolute; top: 95px; left: 50%; margin-left: -56px; }`;
+  css += ` .arrows__btn { background: none; border: 2px solid black; padding: 5px 20px; }`;
+
+  style.appendChild(document.createTextNode(css));
+  divEl.appendChild(style);
+
+  const backBtn = createBackBtn();
+  divEl.appendChild(backBtn);
+
+  const nextBtn = createNextBtn();
+  divEl.appendChild(nextBtn);
+
+  document.body.appendChild(divEl);
+};
+
+addButtonsWithStyles();
+
 const countNextBtnClicks = () => {
   const nextBtn = document.querySelector(`.arrows__wrap button:nth-child(3)`);
 
