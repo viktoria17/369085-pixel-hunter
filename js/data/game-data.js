e16a8d06b9f65b334253a9ded4c6960ed4aa82d6
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-06-23 23:26:08 +0300

    Practice TDD approach, add functions for calculating the result of the game

---
+++ js/data/game-data.js
@@ -0,0 +1,61 @@
+export const countPointsForCorrectAnswers = (answers) => {
+  let score = 0;
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i].isCorrect) {
+      const averageAnswer = answers[i].time > 9 && answers[i].time < 21;
+      const fastAnswer = answers[i].time > 0 && answers[i].time < 10;
+      const slowAnswer = answers[i].time > 20 && answers[i].time < 31;
+
+      if (averageAnswer) {
+        score += 100;
+      } else if (fastAnswer) {
+        score += 150;
+      } else if (slowAnswer) {
+        score += 50;
+      }
+    }
+  }
+
+  return score;
+};
+
+export const gameIsFinished = (answers) => {
+  const amountOfQuestions = 10;
+  const amountOfAnsweredQuestions = answers.length;
+
+  if (amountOfAnsweredQuestions === amountOfQuestions) {
+    return true;
+  }
+
+  const lives = countLives(answers);
+
+  if (lives === -1) {
+    return true;
+  }
+
+  return false;
+};
+
+export const gameIsFailed = (answers) => {
+  const amountOfQuestions = 10;
+  const amountOfAnsweredQuestions = answers.length;
+
+  if (amountOfAnsweredQuestions < amountOfQuestions) {
+    return -1;
+  }
+
+  return false;
+};
+
+export const countLives = (answers) => {
+  let wrongAnswers = 0;
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i].isCorrect === false || answers[i].time > 30) {
+      wrongAnswers += 1;
+    }
+  }
+
+  return 3 - wrongAnswers;
+};
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,9 +1,284 @@
 import {assert} from 'chai';
+import {
+  countPointsForCorrectAnswers,
+  gameIsFinished,
+  gameIsFailed,
+  countLives,
+} from './game-data';
 
-describe(`Array`, () => {
-  describe(`#indexOf()`, () => {
-    it(`should return -1 when the value is not present`, () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
+describe(`Count points`, () => {
+
+  it(`should add 100 points for one correct answer with an average speed`, () => {
+    const answers = [
+      {
+        time: 20,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 100);
+  });
+
+  it(`should add 100 points for each correct answer with an average speed`, () => {
+    const answers = [
+      {
+        time: 10,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 20,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 300);
+  });
+
+  it(`should not add points for incorrect answers`, () => {
+    const answers = [
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 100);
+  });
+
+  it(`should add 150 points for one correct answer with a fast speed`, () => {
+    const answers = [
+      {
+        time: 9,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 150);
+  });
+
+  it(`should add 150 points for each correct answer with a fast speed`, () => {
+    const answers = [
+      {
+        time: 1,
+        isCorrect: true,
+      },
+      {
+        time: 5,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 300);
+  });
+
+  it(`should add 50 points for one correct answer with a slow speed`, () => {
+    const answers = [
+      {
+        time: 21,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 50);
+  });
+
+  it(`should add 50 points for each correct answer with a slow speed`, () => {
+    const answers = [
+      {
+        time: 22,
+        isCorrect: true,
+      },
+      {
+        time: 25,
+        isCorrect: true,
+      },
+      {
+        time: 30,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 150);
+  });
+
+  it(`should calculate correctly answers with different speeds`, () => {
+    const answers = [
+      {
+        time: 5,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 8,
+        isCorrect: true,
+      },
+      {
+        time: 22,
+        isCorrect: true,
+      },
+      {
+        time: 5,
+        isCorrect: false,
+      },
+    ];
+
+    assert.equal(countPointsForCorrectAnswers(answers), 450);
+  });
+
+  it(`should return 1150 points if the player answered all questions with an average speed and he has 3 lives`, () => {
+
+  });
+
+});
+
+describe(`Check if the game is over`, () => {
+  it(`should finish the game if the player answered all questions`, () => {
+    let answers = [];
+
+    for (let i = 0; i < 10; i++) {
+      answers.push({
+        time: 15,
+        isCorrect: true,
+      });
+    }
+
+    assert.equal(gameIsFinished(answers), true);
+  });
+
+  it(`should return -1 if the player answered less than 10 questions`, () => {
+    const answers = [
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(gameIsFailed(answers), -1);
   });
 });
+
+describe(`Count lives`, () => {
+
+  it(`should remain 3 lives if all answers are correct`, () => {
+    const answers = [
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+    ];
+
+    assert.equal(countLives(answers), 3);
+  });
+
+  it(`should remain 1 life if the player made 2 mistakes`, () => {
+    const answers = [
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+    ];
+
+    assert.equal(countLives(answers), 1);
+  });
+
+  it(`should remain 0 lives if the player made 3 mistakes`, () => {
+    const answers = [
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+    ];
+
+    assert.equal(countLives(answers), 0);
+  });
+
+  it(`should end the game if the player made 4 mistakes`, () => {
+    const answers = [
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: true,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+      {
+        time: 15,
+        isCorrect: false,
+      },
+    ];
+
+    assert.equal(countLives(answers), -1);
+    assert.equal(gameIsFinished(answers), true);
+  });
+
+  it(`should take away 1 life if the player did not answer in 30 seconds`, () => {
+    const answers = [
+      {
+        time: 31,
+        isCorrect: ``
+      },
+    ];
+
+    assert.equal(countLives(answers), 2);
+  });
+
+  it(`should not take a life if the player answered in 30 seconds`, () => {
+    const answers = [
+      {
+        time: 30,
+        isCorrect: true
+      },
+    ];
+
+    assert.equal(countLives(answers), 3);
+  });
+});
+
